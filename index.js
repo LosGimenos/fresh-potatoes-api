e9d82b5261e4fe55682364a1fd3481f56deebc62
Larry Hernandez <larryhernanjr@gmail.com>
2018-02-15 20:58:55 -0500

    scaffold relations, response objects

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const sqlite = require('sqlite'),
 
 const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
 
+
+// Sequelize ORM setup
 let sequelize = new Sequelize(null, null, null, {
   host: 'localhost',
   dialect: 'sqlite',
@@ -28,20 +30,25 @@ sequelize
     console.log('Unable to connect to the database:', err);
   });
 
+// Sequelize Models
 const Films = sequelize.define('films', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true
+    },
+    title: Sequelize.STRING,
+    release_date: Sequelize.STRING,
+    tagline: Sequelize.STRING,
+    revenue: Sequelize.INTEGER,
+    budget: Sequelize.INTEGER,
+    runtime: Sequelize.INTEGER,
+    original_language: Sequelize.STRING,
+    status: Sequelize.STRING
   },
-  title: Sequelize.STRING,
-  release_date: Sequelize.STRING,
-  tagline: Sequelize.STRING,
-  revenue: Sequelize.INTEGER,
-  budget: Sequelize.INTEGER,
-  runtime: Sequelize.INTEGER,
-  original_language: Sequelize.STRING,
-  status: Sequelize.STRING
-});
+  {
+    underscored: true
+  }
+);
 
 const Artists = sequelize.define('artists', {
     id: {
@@ -81,29 +88,49 @@ const ArtistFilms = sequelize.define('artist_films', {
   }
 );
 
+// Map Sequelize Models relationships
+Films.belongsTo(Genres);
+ArtistFilms.belongsTo(Artists);
+ArtistFilms.belongsTo(Films);
 
-
+// Sequelize sync
 sequelize.sync().then(() => {
+  // START SERVER
   Promise.resolve()
     .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
     .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
 
+  // ROUTES
   app.get('/films/:id/recommendations', getFilmRecommendations);
 });
 
-// START SERVER
-
-
-// ROUTES
-
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  console.log('made it here')
+  res.type('json');
+  res.header('Content-Type', 'application/json');
+
+  let responseObject = {
+    recommendations: []
+  };
+
+  let errorObject = {
+    message: null
+  };
+
   let filmId = req.params.id;
-  Genres.findById(filmId).then(genre => {
-    console.log(genre, 'this is genre');
-    res.status(200).send(genre);
-  });
+
+  Films
+    .findById(filmId)
+    .then(film => {
+      return film.dataValues;
+    })
+    .then(filmResponse => {
+      responseObject['recommendations'].push(filmResponse);
+      res.status(200).json(responseObject);
+    })
+    .catch(err => {
+      res.status(422).json(errorObject);
+    })
 }
 
 module.exports = app;
